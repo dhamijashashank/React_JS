55e019333113eb99843582571d6008d79a00ea6b
dhamija.shashank <dhamija.shashank@gmail.com>
2017-07-11 17:40:43 +0530

    Having Example of Event and Unidirectional flow

--- example_3_event_unidir/src/index.js
+++ example_3_event_unidir/src/index.js
@@ -1,8 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class ToDoList extends React.Component{
+
+    constructor(){
+        super();
+        this.ChangeStatus = this.ChangeStatus.bind(this);
+        this.state = {
+            tasks: [{
+               name:"Bulk Milk",
+               completed:"false" 
+            },
+            {
+               name:"Bulk Test",
+               completed:"false" 
+            },
+            {
+               name:"Alone Testing",
+               completed:"false" 
+            }
+            ]
+        }
+    }
+
+    ChangeStatus(index){
+       var tasks = this.state.tasks;
+       var task = tasks[index];
+       task.completed = !task.completed;
+       this.setState({
+           tasks : tasks
+       })
+       console.log(this.state.tasks[index])
+    }
+
+render(){
+    return (
+        <ul>
+            {
+                this.state.tasks.map((task , index) => {
+                return <ToDoItem key={task.name} clickHandler={this.ChangeStatus} index={index} deta={task} />
+                }
+                )
+            }
+            </ul>
+        )   
+    }
+}
+class ToDoItem extends React.Component {
+
+render(){
+    return <li onClick={ () => {
+            this.props.clickHandler(this.props.index)
+        }} className ={this.props.deta.completed ?'completed' :' '}>
+        
+            {this.props.deta.name}
+        </li>
+}
+}
+ReactDOM.render(<ToDoList/>, document.getElementById('root'));
