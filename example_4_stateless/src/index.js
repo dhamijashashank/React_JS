58e070de970d5ab6116e180a6812a0d81cbf72ce
dhamija.shashank <dhamija.shashank@gmail.com>
2017-07-12 09:04:49 +0530

    Add Example of StateLess Function Example_4

---
+++ example_4_stateless/src/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import ToDoItem from './ToDoItem.js'
+
+class ToDoList extends React.Component{
+
+    constructor(){
+        super();
+        this.ChangeStatus = this.ChangeStatus.bind(this);
+        this.state = {
+            tasks: [{
+               name:"Bulk Milk",
+               completed:"false" 
+            },
+            {
+               name:"Bulk Test",
+               completed:"false" 
+            },
+            {
+               name:"Alone Testing",
+               completed:"false" 
+            }
+            ]
+        }
+    }
+
+    ChangeStatus(index){
+       var tasks = this.state.tasks;
+       var task = tasks[index];
+       task.completed = !task.completed;
+       this.setState({
+           tasks : tasks
+       })
+       console.log(this.state.tasks[index])
+    }
+
+render(){
+    return (
+        <ul>
+            {
+                this.state.tasks.map((task , index) => {
+                return <ToDoItem key={task.name} clickHandler={this.ChangeStatus} index={index} deta={task} />
+                }
+                )
+            }
+            </ul>
+        )   
+    }
+}
+
+ReactDOM.render(<ToDoList/>, document.getElementById('root'));
